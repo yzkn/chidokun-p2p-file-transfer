d58f50978cf33f7eb86497c0d8617e36b241a3de
YA <9039417+yzkn@users.noreply.github.com>
2025-05-21 21:59:47 +0900

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Button, Card, Col, Input, Menu, MenuProps, message, Row, Space, Typography, Upload, UploadFile} from "antd";
-import {CopyOutlined, UploadOutlined} from "@ant-design/icons";
-import {useAppDispatch, useAppSelector} from "./store/hooks";
-import {startPeer, stopPeerSession} from "./store/peer/peerActions";
+import { Button, Card, Col, Input, Menu, MenuProps, message, Row, Space, Typography, Upload, UploadFile } from "antd";
+import { CopyOutlined, UploadOutlined } from "@ant-design/icons";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { startPeer, stopPeerSession } from "./store/peer/peerActions";
 import * as connectionAction from "./store/connection/connectionActions"
-import {DataType, PeerConnection} from "./helpers/peer";
-import {useAsyncState} from "./helpers/hooks";
+import { DataType, PeerConnection } from "./helpers/peer";
+import { useAsyncState } from "./helpers/hooks";
 
-const {Title} = Typography
+const { Title } = Typography
 type MenuItem = Required<MenuProps>['items'][number]
 
 function getItem(
@@ -60,7 +60,7 @@ export const App: React.FC = () => {
         try {
             await setSendLoading(true);
             let file = fileList[0] as unknown as File;
-            let blob = new Blob([file], {type: file.type});
+            let blob = new Blob([file], { type: file.type });
 
             await PeerConnection.sendConnection(connection.selectedId, {
                 dataType: DataType.FILE,
@@ -81,66 +81,66 @@ export const App: React.FC = () => {
         <Row justify={"center"} align={"top"}>
             <Col xs={24} sm={24} md={20} lg={16} xl={12}>
                 <Card>
-                    <Title level={2} style={{textAlign: "center"}}>P2P File Transfer</Title>
-                        <Card hidden={peer.started}>
-                            <Button onClick={handleStartSession} loading={peer.loading}>Start</Button>
-                        </Card>
-                        <Card hidden={!peer.started}>
+                    <Title level={2} style={{ textAlign: "center" }}>File Transfer</Title>
+                    <Card hidden={peer.started}>
+                        <Button onClick={handleStartSession} loading={peer.loading}>Start</Button>
+                    </Card>
+                    <Card hidden={!peer.started}>
+                        <Space direction="horizontal">
+                            <div>ID: {peer.id}</div>
+                            <Button icon={<CopyOutlined />} onClick={async () => {
+                                await navigator.clipboard.writeText(peer.id || "")
+                                message.info("Copied: " + peer.id)
+                            }} />
+                            <Button danger onClick={handleStopSession}>Stop</Button>
+                        </Space>
+                    </Card>
+                    <div hidden={!peer.started}>
+                        <Card>
                             <Space direction="horizontal">
-                                <div>ID: {peer.id}</div>
-                                <Button icon={<CopyOutlined/>} onClick={async () => {
-                                    await navigator.clipboard.writeText(peer.id || "")
-                                    message.info("Copied: " + peer.id)
-                                }}/>
-                                <Button danger onClick={handleStopSession}>Stop</Button>
+                                <Input placeholder={"ID"}
+                                    onChange={e => dispatch(connectionAction.changeConnectionInput(e.target.value))}
+                                    required={true}
+                                />
+                                <Button onClick={handleConnectOtherPeer}
+                                    loading={connection.loading}>Connect</Button>
                             </Space>
                         </Card>
-                        <div hidden={!peer.started}>
-                            <Card>
-                                <Space direction="horizontal">
-                                    <Input placeholder={"ID"}
-                                           onChange={e => dispatch(connectionAction.changeConnectionInput(e.target.value))}
-                                           required={true}
-                                           />
-                                    <Button onClick={handleConnectOtherPeer}
-                                            loading={connection.loading}>Connect</Button>
-                                </Space>
-                            </Card>
 
-                            <Card title="Connection">
-                                {
-                                    connection.list.length === 0
-                                        ? <div>Waiting for connection ...</div>
-                                        : <div>
-                                            Select a connection
-                                            <Menu selectedKeys={connection.selectedId ? [connection.selectedId] : []}
-                                                  onSelect={(item) => dispatch(connectionAction.selectItem(item.key))}
-                                                  items={connection.list.map(e => getItem(e, e, null))}/>
-                                        </div>
-                                }
+                        <Card title="Connection">
+                            {
+                                connection.list.length === 0
+                                    ? <div>Waiting for connection ...</div>
+                                    : <div>
+                                        Select a connection
+                                        <Menu selectedKeys={connection.selectedId ? [connection.selectedId] : []}
+                                            onSelect={(item) => dispatch(connectionAction.selectItem(item.key))}
+                                            items={connection.list.map(e => getItem(e, e, null))} />
+                                    </div>
+                            }
 
-                            </Card>
-                            <Card title="Send File">
-                                <Upload fileList={fileList}
-                                        maxCount={1}
-                                        onRemove={() => setFileList([])}
-                                        beforeUpload={(file) => {
-                                            setFileList([file])
-                                            return false
-                                        }}>
-                                    <Button icon={<UploadOutlined/>}>Select File</Button>
-                                </Upload>
-                                <Button
-                                    type="primary"
-                                    onClick={handleUpload}
-                                    disabled={fileList.length === 0}
-                                    loading={sendLoading}
-                                    style={{marginTop: 16}}
-                                >
-                                    {sendLoading ? 'Sending' : 'Send'}
-                                </Button>
-                            </Card>
-                        </div>
+                        </Card>
+                        <Card title="Send File">
+                            <Upload fileList={fileList}
+                                maxCount={1}
+                                onRemove={() => setFileList([])}
+                                beforeUpload={(file) => {
+                                    setFileList([file])
+                                    return false
+                                }}>
+                                <Button icon={<UploadOutlined />}>Select File</Button>
+                            </Upload>
+                            <Button
+                                type="primary"
+                                onClick={handleUpload}
+                                disabled={fileList.length === 0}
+                                loading={sendLoading}
+                                style={{ marginTop: 16 }}
+                            >
+                                {sendLoading ? 'Sending' : 'Send'}
+                            </Button>
+                        </Card>
+                    </div>
                 </Card>
             </Col>
 
